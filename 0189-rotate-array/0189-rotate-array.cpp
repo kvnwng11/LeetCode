a89bb516cfb257495a4dac0eb22769267a3f73eb
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-09-14 22:57:06 -0400

    Time: 24 ms (51.23%), Space: 25.5 MB (40.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         k = k % nums.size();
-        int count = 0;
-        for (int start = 0; count < nums.size(); ++start) {
-            int current = start;
+        int N = 0;
+        int start = 0;
+
+        while (N < nums.size()) {
+            int curr = start;
             int prev = nums[start];
+
             do {
-                int next = (current + k) % nums.size();
+                int next = (curr + k) % nums.size();
                 int tmp = nums[next];
                 nums[next] = prev;
                 prev = tmp;
-                current = next;
-                count++;
-            } while (start != current);
+                curr = next;
+                N++;
+            } while(curr != start);
+
+            start++;
         }
     }
 };
