ffd57f8d0d36688f9cbd86f0693a49bd9c73d61f
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-09-14 21:51:35 -0400

    Time: 11 ms (70.23%), Space: 20.4 MB (34.81%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,15 +4,11 @@ public:
         if (height.size() <= 2)
             return 0;
 
-        vector<int> leftMax(height.size());
+        int leftMax = height[0];
         vector<int> rightMax(height.size());
         int ans = 0;
 
         // Initialize
-        leftMax[0] = height[0];
-        for (int i=1; i<height.size(); ++i) {
-            leftMax[i] = max(height[i], leftMax[i-1]);
-        }
         rightMax[height.size()-1] = height[height.size()-1];
         for (int i=height.size()-2; i>=0; --i) {
             rightMax[i] = max(height[i], rightMax[i+1]);
@@ -20,7 +16,8 @@ public:
 
         // Search
         for (int i=1; i<height.size()-1; ++i) {
-            ans += min(leftMax[i], rightMax[i]) - height[i];
+            leftMax = max(height[i], leftMax);
+            ans += min(leftMax, rightMax[i]) - height[i];
         }
 
         return ans;
