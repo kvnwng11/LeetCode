4c53dc4e63ed70e195b28046ba611dc77a650658
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-09-14 20:44:54 -0400

    Time: 32 ms (38.39%), Space: 19.7 MB (13.14%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.cpp
+++ 1028-interval-list-intersections/1028-interval-list-intersections.cpp
@@ -6,31 +6,23 @@ public:
 
         int p1 = 0, p2 = 0;
         while (p1 < firstList.size() and p2 < secondList.size()) {
-            // if Disjoint
-            if (firstList[p1][1] < secondList[p2][0]) {
-                p1++;
-                continue;
-            }
-            if (secondList[p2][1] < firstList[p1][0]) {
-                p2++;
-                continue;
-            }
-
             // Initialize interval
-            vector<int> currInterval(2);
-            currInterval[0] = max(firstList[p1][0], secondList[p2][0]);
-            currInterval[1] = min(firstList[p1][1], secondList[p2][1]);
+            int low = max(firstList[p1][0], secondList[p2][0]);
+            int high = min(firstList[p1][1], secondList[p2][1]);
+
+            if (low <= high) {
+                vector<int> currInterval(2);
+                currInterval[0] = low;
+                currInterval[1] = high;
+                ans.push_back(currInterval);
+            }
 
             // Case 1
-            if (firstList[p1][1] < secondList[p2][1]) {
+            if (firstList[p1][1] < secondList[p2][1]) 
                 p1++;
-            }
             // Case 2
-            else {
+            else 
                 p2++;
-            }
-
-            ans.push_back(currInterval);
         }
 
         return ans;
