981fa9f4d07c945b3a1d6dc813636145dc383d33
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-09-25 20:11:02 -0400

    Time: 140 ms (89.28%), Space: 111.6 MB (56.84%) - LeetHub

---
+++ 1792-find-the-most-competitive-subsequence/1792-find-the-most-competitive-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    void helper() {
+
+    }
+
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        vector<int> ans;
+        if (nums.size() == k) return nums;
+        int maxPop = nums.size() - k;
+        for (int i=0; i<nums.size(); ++i) {
+            while (maxPop > 0 and ans.size() and ans.back() > nums[i]) {
+                ans.pop_back();
+                maxPop--;
+            }
+            
+            ans.push_back(nums[i]);
+        }
+
+        while (maxPop--) {
+            ans.pop_back();
+        }
+
+        return ans;
+    }
+};
