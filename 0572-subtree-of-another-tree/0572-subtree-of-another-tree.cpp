99d851bc7f4e12b14d6ba88c8230326384c78c19
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-09-27 21:07:50 -0400

    Time: 14 ms (84.58%), Space: 29.7 MB (7.38%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,16 +11,33 @@
  */
 class Solution {
 private:
+    vector<TreeNode*> nodes;
+
     bool isSame(TreeNode* p, TreeNode* q) {
         if (!p and !q) return 1;
         if (!p or !q) return 0;
         if (p->val != q->val) return 0;
         return isSame(p->left, q->left) and isSame(p->right, q->right);
     }
+
+    int getDepth(TreeNode* n, int target) {
+        if (!n) return -1;
+        int depth = max(getDepth(n->left, target), getDepth(n->right, target)) + 1;
+
+        if (depth == target)
+            nodes.push_back(n);
+        
+        return depth;
+    }
+
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (!root) return 0;
-        if (isSame(root, subRoot)) return 1;
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        if (!root and !subRoot) return 1;
+        if (!root or !subRoot) return 0;
+        getDepth(root, getDepth(subRoot, -1));
+        for (TreeNode* node : nodes)
+            if (isSame(subRoot, node))
+                return 1;
+        return 0;
     }
 };
