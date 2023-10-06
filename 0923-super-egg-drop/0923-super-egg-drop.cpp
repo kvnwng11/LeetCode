abf812b6fb678e672cbd083a375462fb415f77ac
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-10-06 11:42:26 -0400

    Time: 68 ms (61.63%), Space: 47.7 MB (5.82%) - LeetHub

--- 0923-super-egg-drop/0923-super-egg-drop.cpp
+++ 0923-super-egg-drop/0923-super-egg-drop.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     int superEggDrop(int k, int n) {
-        vector<vector<int>> dp(k+1, vector<int>(n+1, 0));
+        vector<vector<int>> dp(n+1, vector<int>(k+1, 0));
 
-        for (int i=0; i<=n; ++i) {
-            dp[1][i] = i;
-        }
-
-        for (int i=2; i<=k; ++i) {
-            for (int j=1; j<=n; ++j) {
-                int ans = INT_MAX;
-                int left = 1, right = j;
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
-
-                    int lo = dp[i-1][mid-1], hi = dp[i][j-mid];
-                    int temp = 1 + max(lo, hi);
-                    ans = min(ans, temp);
-                    if (lo < hi)
-                        left = mid+1;
-                    else
-                        right = mid-1;
-                }
-                dp[i][j] = ans;
-                //cout << dp[i][j] << " ";
+        int m = 0;
+        while (dp[m][k] < n) {
+            m++;
+            for (int i=1; i<=k; ++i) {
+                dp[m][i] = dp[m-1][i-1] + dp[m-1][i] + 1;
             }
-            //cout << endl;
         }
 
-        return dp[k][n];
+        return m;
     }
 };
