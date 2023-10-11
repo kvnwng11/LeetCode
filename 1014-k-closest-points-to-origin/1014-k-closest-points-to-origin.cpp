3ed65e81fd70aa05aefda0678d804a01b04daebf
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-10-11 14:25:17 -0400

    Time: 242 ms (45.83%), Space: 76.6 MB (33.91%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,26 +1,32 @@
 class Solution {
 private:
 
-    struct myCompare {
+    struct MyComp{
         double dist(vector<int> &v) {
-            return v[0]*v[0] + v[1]*v[1];
+            int x = v[0], y = v[1];
+            return x*x + y*y;
         }
+
         bool operator() (vector<int> &v1, vector<int> &v2) {
             return dist(v1) > dist(v2);
         }
     };
-
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<vector<int>, vector<vector<int>>, myCompare> pq;
+        priority_queue<vector<int>, vector<vector<int>>, MyComp> q;
+
         for (int i=0; i<points.size(); ++i) {
-            pq.push(points[i]);
+            q.push(points[i]);
         }
+
         vector<vector<int>> ans;
-        while (k--) {
-            ans.push_back(pq.top());
-            pq.pop();
+
+        for (int i=0; i<k; ++i) {
+            ans.push_back(q.top());
+            q.pop();
         }
+
+
         return ans;
     }
 };
