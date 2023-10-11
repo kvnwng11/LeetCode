d8b628bcd39a4bcc3f02b100a75b73fcd96ecda9
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-10-11 15:17:52 -0400

    Time: 222 ms (60.89%), Space: 122.5 MB (55.71%) - LeetHub

--- 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.cpp
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
         deque<int> d;
-        vector<int> dp(nums.size());
+        vector<int> sum(nums.size());
         int ans = INT_MIN;
 
         for (int i=0; i<nums.size(); ++i) {
@@ -10,16 +10,16 @@ public:
                 d.pop_front();
             }
 
-            dp[i] = (!d.empty()? dp[d.front()] : 0) + nums[i];
+            sum[i] = (!d.empty()? sum[d.front()] : 0) + nums[i];
 
-            while (!d.empty() and dp[d.back()] < dp[i]) {
+            while (!d.empty() and sum[d.back()] < sum[i]) {
                 d.pop_back();
             }
 
-            if (dp[i] > 0)
+            if (sum[i] > 0)
                 d.push_back(i);
 
-            ans = max(dp[i], ans);
+            ans = max(sum[i], ans);
         }
 
         return ans;
