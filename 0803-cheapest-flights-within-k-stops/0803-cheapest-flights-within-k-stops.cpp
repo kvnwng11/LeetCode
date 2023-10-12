b56f18a8f185eb6312b28c2d35f7c763b552c159
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-10-12 14:23:14 -0400

    Time: 23 ms (33.22%), Space: 15.2 MB (16.79%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist(n+1, INT_MAX);
-        dist[src] = 0;
-        for (int i=0; i<=k; ++i) {
-            vector<int> temp(dist);
-            for (auto &flight : flights) {
-                int from = flight[0];
-                int to = flight[1];
-                int price = flight[2];
-                if (dist[from] != INT_MAX) {
-                    temp[to] = min(temp[to], dist[from] + price);
-                }
-            }
-            dist = temp;
+        // Create Adj List
+        unordered_map<int, vector<pair<int, int>>> adjList;
+        for (auto &flight : flights) {
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+
+            adjList[from].push_back({to, price});
         }
 
-        return dist[dst] == INT_MAX? -1 : dist[dst];
+        // Dijkstra's
+        vector<int> stops(n+1, INT_MAX);
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({0, src, 0});
+
+        while (!pq.empty()) {
+            int dist = pq.top()[0];
+            int node = pq.top()[1];
+            int numStops = pq.top()[2];
+            pq.pop();
+
+            // Already found a better path
+            if (numStops > stops[node] or numStops > k + 1) 
+                continue;
+
+            // Done
+            if (node == dst)
+                return dist;
+
+            stops[node] = numStops;
+            for (auto &[neighbor, price] : adjList[node]) {
+                pq.push({dist + price, neighbor, numStops+1});
+            }
+            
+        }
+
+        return -1;
     }
 };
