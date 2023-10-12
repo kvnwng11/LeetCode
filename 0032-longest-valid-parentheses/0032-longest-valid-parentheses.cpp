d3cdc9258da0d2fa5bd7cdf6a418d7ce46b6b531
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-10-11 23:27:35 -0400

    Time: 0 ms (100%), Space: 7.1 MB (89.02%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,24 +2,39 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         if (!s.size()) return 0;
-        stack<int> st;
-        int ans = 0, trash = -1;
+        int left = 0, right = 0;
+        int ans = 0;
         for (int i=0; i<s.size(); ++i) {
             if (s[i] == '(') {
-                st.push(i);
+                left++;
             }
             else {
-                if (st.empty()) {
-                    trash = i;
-                    continue;
-                }
+                right++;
+            }
 
-                st.pop();
+            if (right > left) {
+                left = 0;
+                right = 0;
+            }
+            else if (right == left) {
+                ans = max(2 * right, ans);
+            }
+        }
+        left = right = 0;
+        for (int i=s.size()-1; i>=0; --i) {
+            if (s[i] == '(') {
+                left++;
+            }
+            else {
+                right++;
+            }
 
-                if (st.empty())
-                    ans = max(ans, i - trash);
-                else
-                    ans = max(ans, i - st.top());
+            if (right < left) {
+                left = 0;
+                right = 0;
+            }
+            else if (right == left) {
+                ans = max(2 * right, ans);
             }
         }
 
