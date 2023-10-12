ff1989f24e393335ede508ec9fb1a840fc37f18e
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-10-12 14:35:59 -0400

    Time: 333 ms (17.06%), Space: 34.3 MB (22.95%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,36 +1,54 @@
 class Solution {
 private:
     typedef vector<int> vi;
+    int add[4][2] = {
+        {0, 1},
+        {1, 0},
+        {-1, 0},
+        {0, -1}
+    };
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n = heights.size(), m = heights[0].size();
-        int add[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        int ans = INT_MAX;
-        vector<vector<int>> costs(n, vector<int>(m, INT_MAX));
-        priority_queue<vi, vector<vi>, greater<vi>> pq;
+        // Declare variables
+        int n = heights.size();
+        int m = heights[0].size();
 
-        pq.push({0, 0, 0});
+        // Min distance to reach this cell
+        vector<vector<int>> costs(n, vector<int>(m, INT_MAX)); 
+        costs[0][0] = 0;
+
+        // Dijkstras
+        priority_queue<vi, vector<vi>, greater<vi>> pq;
+        pq.push({0, 0, 0}); // [dist, row, col]
+
         while (!pq.empty()) {
             int dist = pq.top()[0];
             int row = pq.top()[1];
             int col = pq.top()[2];
             pq.pop();
 
-            if (row == n-1 and col == m-1) return dist;
-            if (dist >= costs[row][col]) continue;
-            costs[row][col] = dist;
+            // Reached dest
+            if (row == n-1 and col == m-1)
+                return dist;
 
+            // Add neighbors
             for (int i=0; i<4; ++i) {
                 int newRow = row + add[i][0];
                 int newCol = col + add[i][1];
 
-                if (newRow >= 0 and newRow < n and newCol >= 0 and newCol < m) {
-                    int newDist = max(dist, abs(heights[row][col] - heights[newRow][newCol]));
+                if (newRow < 0 or newRow >= n) continue;
+                if (newCol < 0 or newCol >= m) continue;
+
+                int newDist = max(dist, abs(heights[row][col] - heights[newRow][newCol]));
+
+                if (newDist < costs[newRow][newCol]) {
+                    costs[newRow][newCol] = newDist;
                     pq.push({newDist, newRow, newCol});
                 }
             }
+
         }
 
-        return costs[n-1][m-1];
+        return 0;
     }
 };
