1667bcb6bc24877923fb279ab7dfb2f6ded94706
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-10-22 14:48:39 -0400

    Time: 76 ms (16.76%), Space: 33.6 MB (64.35%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -3,29 +3,14 @@ public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
         int n = rowSum.size();
         int m = colSum.size();
-        vector<int> currRowSum(n, 0);
-        vector<int> currColSum(m, 0);
         vector<vector<int>> ans(n, vector<int>(m, 0));
 
         for (int i=0; i<n; ++i) {
-            int toAdd = rowSum[i];
-
             for (int j=0; j<m; ++j) {
-                //cout << toAdd << endl;
-                // Overflow
-                if (toAdd + currColSum[j] > colSum[j]) {
-                    ans[i][j] = colSum[j] - currColSum[j];
-                    toAdd -= (colSum[j] - currColSum[j]);
-                    currColSum[j] = colSum[j];
-                }
-                // Otherwise
-                else {
-                    ans[i][j] = toAdd;
-                    currColSum[j] += toAdd;
-                    toAdd = 0;
-                }
+                ans[i][j] = min(rowSum[i], colSum[j]);
+                rowSum[i] -= ans[i][j];
+                colSum[j] -= ans[i][j];
             }
-            //cout << endl;
         }
 
 
