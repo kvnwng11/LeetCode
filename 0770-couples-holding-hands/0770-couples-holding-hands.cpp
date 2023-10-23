6d3fb64c7b0379e991a521b3805831b3dc847dcd
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-10-23 11:49:09 -0400

    Time: 4 ms (24.2%), Space: 7.8 MB (34.35%) - LeetHub

--- 0770-couples-holding-hands/0770-couples-holding-hands.cpp
+++ 0770-couples-holding-hands/0770-couples-holding-hands.cpp
@@ -3,6 +3,11 @@ public:
     int minSwapsCouples(vector<int>& row) {
         int ans = 0;
 
+        unordered_map<int, int> idx;
+        for (int i=0; i<row.size(); ++i) {
+            idx[row[i]] = i;
+        }
+
         for (int i=0; i<row.size(); i += 2) {
             int x = row[i];
             int partner = x % 2 == 0? x+1 : x-1;
@@ -11,11 +16,13 @@ public:
 
             ans++;
 
-            for (int j=i+1; j<row.size(); ++j) {
-                if (row[j] == partner) {
-                    row[j] = row[i+1];
-                    row[i+1] = partner;
-                }
+            int j = idx[partner];
+            if (j > i) {
+                row[j] = row[i+1];
+                row[i+1] = partner;
+
+                idx[partner] = i+1;
+                idx[row[j]] = j;
             }
         }
 
