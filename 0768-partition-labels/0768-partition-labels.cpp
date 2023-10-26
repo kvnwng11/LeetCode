4a0399ba84ed7542f873802c46af7d754308c704
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-10-26 11:37:57 -0400

    Time: 7 ms (14.78%), Space: 7.3 MB (18.42%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        // Get start and ends
+        unordered_map<char, int> starts;
+        unordered_map<char, int> ends;
+        for (int i=0; i<s.size(); ++i) {
+            if (!starts.count(s[i])) {
+                starts[s[i]] = i;
+            }
+            ends[s[i]] = i;
+        }
+
+        vector<int> ans;
+        int endOfSegment = ends[s[0]];
+        int count = 0;
+        for (int i=0; i<s.size(); ++i) {
+            if (i > endOfSegment) {
+                ans.push_back(count);
+                endOfSegment = ends[s[i]];
+                count = 1;
+            }
+            else {
+                count++;
+                endOfSegment = max(endOfSegment, ends[s[i]]);
+            }
+        }
+        ans.push_back(count);
+
+        return ans;
+    }
+};
