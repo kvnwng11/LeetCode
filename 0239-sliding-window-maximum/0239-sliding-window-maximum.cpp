14fdb21905a73844ebb479237688b13dfb8e353f
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-11-11 11:55:37 -0500

    Time: 316 ms (18.99%), Space: 155.5 MB (18.08%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        priority_queue<int> pq;
         vector<int> ans;
-        deque<int> d;
-        unordered_map<int, int> window;
 
-        // First iteration
-        for (int i=0; i<k; ++i) {
-            while (!d.empty() and d.back() <= nums[i]) {
-                d.pop_back();
+        for (int i=0; i<nums.size(); ++i) {
+            pq.push(nums[i]);
+            counts[nums[i]] += 1;
+
+            if (pq.size() >= k) {
+
+                while (counts[pq.top()] == 0)
+                    pq.pop();
+
+                ans.push_back(pq.top());
+
+                counts[nums[i-k+1]] -= 1;
             }
-            d.push_back(nums[i]);
-            window[nums[i]] += 1;
         }
-        ans.push_back(d.front());
 
-        // Other iterations
-        for (int i=k; i<nums.size(); ++i) {
-            // Add to queue
-            while (!d.empty() and d.back() <= nums[i]) {
-                d.pop_back();
-            }
-            d.push_back(nums[i]);
-
-            // Update window
-            window[nums[i]] += 1;
-            window[nums[i - k]] -= 1;
-
-            // Remove elements not in window
-            while (!d.empty() and window[d.front()] == 0) {
-                d.pop_front();
-            }
-
-            // Update ans
-            ans.push_back(d.front());
-        }
 
         return ans;
     }
