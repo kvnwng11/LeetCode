092512292bb30806847b11eb7361c13c99a7303d
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-11-13 12:20:36 -0500

    Time: 43 ms (11.34%), Space: 14.2 MB (30.94%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,42 +1,46 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // Create Adj List
-        unordered_map<int, vector<pair<int, int>>> adjList;
+        unordered_map<int, vector<pair<int, double>>> adjList;
         for (auto &flight : flights) {
             int from = flight[0];
             int to = flight[1];
-            int price = flight[2];
+            double price = flight[2];
 
             adjList[from].push_back({to, price});
         }
 
-        // Dijkstra's
-        vector<int> stops(n+1, INT_MAX);
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0, src, 0});
+        vector<double> prices(n, DBL_MAX);
+        prices[src] = 0;
 
-        while (!pq.empty()) {
-            int dist = pq.top()[0];
-            int node = pq.top()[1];
-            int numStops = pq.top()[2];
-            pq.pop();
+        //[[0,1,1],[0,2,5],[1,2,1],[2,3,1]]
 
-            // Already found a better path
-            if (numStops > stops[node] or numStops > k + 1) 
-                continue;
+        for (int i=0; i<=k; ++i) {
+            vector<double> newPrices(prices);
 
-            // Done
-            if (node == dst)
-                return dist;
+            for (int node=0; node<n; ++node) {
+                if (prices[node] == DBL_MAX)
+                    continue;
 
-            stops[node] = numStops;
-            for (auto &[neighbor, price] : adjList[node]) {
-                pq.push({dist + price, neighbor, numStops+1});
+                for (auto &pp : adjList[node]) {
+                    int to = pp.first;
+                    double p = pp.second;
+
+                    newPrices[to] = min(newPrices[to], prices[node] + p);
+                }
             }
-            
+
+            prices = newPrices;
+
+            /**/
+            for (int j=0; j<n; ++j) {
+                cout << prices[j] << " ";
+            }
+            cout << endl;
+            /**/
         }
 
-        return -1;
+
+        return prices[dst] == DBL_MAX? -1 : prices[dst];
     }
 };
