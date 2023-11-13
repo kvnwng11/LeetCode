0d6dcb5b3c3cae5484e567cb4deaee1a5e9492e7
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-11-13 12:53:58 -0500

    Time: 28 ms (22.6%), Space: 12.9 MB (95.64%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        unordered_map<int, vector<pair<int, double>>> adjList;
-        for (auto &flight : flights) {
-            int from = flight[0];
-            int to = flight[1];
-            double price = flight[2];
-
-            adjList[from].push_back({to, price});
-        }
-
-        vector<double> prices(n, DBL_MAX);
+        vector<int> prices(n, INT_MAX);
         prices[src] = 0;
 
         //[[0,1,1],[0,2,5],[1,2,1],[2,3,1]]
 
         for (int i=0; i<=k; ++i) {
-            vector<double> newPrices(prices);
+            vector<int> newPrices(prices);
 
-            for (int node=0; node<n; ++node) {
-                if (prices[node] == DBL_MAX)
-                    continue;
+            for (auto &flight : flights) {
+                int from = flight[0];
+                int to = flight[1];
+                int p = flight[2];
 
-                for (auto &pp : adjList[node]) {
-                    int to = pp.first;
-                    double p = pp.second;
-
-                    newPrices[to] = min(newPrices[to], prices[node] + p);
+                if (prices[from] != INT_MAX) {
+                    newPrices[to] = min(newPrices[to], prices[from] + p);
                 }
             }
 
             prices = newPrices;
-
-            /**/
-            for (int j=0; j<n; ++j) {
-                cout << prices[j] << " ";
-            }
-            cout << endl;
-            /**/
         }
 
 
-        return prices[dst] == DBL_MAX? -1 : prices[dst];
+        return prices[dst] == INT_MAX? -1 : prices[dst];
     }
 };
