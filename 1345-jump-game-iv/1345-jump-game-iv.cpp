6296d5d341b0c8c7c97f4f73df23f507ab4b3fa7
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-11-22 18:02:13 -0500

    Time: 356 ms (10.33%), Space: 95 MB (34.90%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, vector<int>> adjList;
+        
+        for (int i=0; i<arr.size(); ++i) {
+            int val = arr[i];
+            adjList[val].push_back(i);
+        }
+        
+        for (auto &[val, a] : adjList) {
+            cout << val << endl;
+            for (int n : a) {
+                cout << n << " ";
+            }
+            cout << endl << endl;
+        }
+        
+        vector<bool> visited(n, 0);
+        visited[0] = 1;
+        
+        queue<int> q;
+        q.push(0);
+        
+        int ans = 0;
+        
+        
+        while (!q.empty()) {
+            int size = q.size();
+            
+            for (int i=0; i<size; ++i) {
+                int curr = q.front();
+                q.pop();
+                
+                //cout << curr << endl;
+
+                if (curr == n-1)
+                    return ans;
+            
+                for (int neighbor : adjList[arr[curr]]) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = 1;
+                        q.push(neighbor);
+                    }
+                }
+
+                adjList.erase(arr[curr]);
+
+                if (curr+1 < n and !visited[curr+1]) {
+                    visited[curr+1] = 1;
+                    q.push(curr+1);
+                }
+                if (curr-1 >= 0 and !visited[curr-1]) {
+                    visited[curr-1] = 1;
+                    q.push(curr-1);
+                }
+            }
+            
+            ans++;
+        
+        }
+        
+        return -1;
+    }
+};
