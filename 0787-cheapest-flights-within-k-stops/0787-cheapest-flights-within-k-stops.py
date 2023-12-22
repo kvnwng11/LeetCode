7a77e8cefb43daf37ac52d6fb449433c28f01037
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2023-12-21 19:52:15 -0500

    Time: 180 ms (38.46%), Space: 18.5 MB (9.74%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,35 +1,17 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        adj = {}
-        for f, t, price in flights:
-            if f not in adj:
-                adj[f] = []
-                
-            adj[f].append((t, price))
-        
+
         dists = [math.inf] * n
+        dists[src] = 0
         
-        # BFS
-        q = deque()
-        q.append((src, 0)) # (node, dist)
-        
-        stops = 0
-        
-        while stops <= k and q:
-            size = len(q)
+        for i in range(k+1):
+            temp = dists.copy()
             
-            for i in range(size):
-                node, dist = q.popleft()
-                
-                if node not in adj:
-                    continue
+            for f, t, price in flights:
+                if dists[f] != math.inf:
+                    temp[t] = min(temp[t], dists[f] + price)
                     
-                for neighbor, price in adj[node]:
-                    if price + dist < dists[neighbor]:
-                        dists[neighbor] = price + dist
-                        q.append((neighbor, price + dist))
-            
-            stops += 1
+            dists = temp.copy()
         
         return -1 if dists[dst] == math.inf else dists[dst]
         
