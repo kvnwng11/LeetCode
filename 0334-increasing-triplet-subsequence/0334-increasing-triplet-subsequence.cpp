e5040fbd8d3b4667909e2572b9bbd15c7f6b57b3
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-12 12:49:17 -0700

    Time: 100 ms (8.46%), Space: 114.4 MB (66.63%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        if (nums.size() < 3) return 0;
+        int low = INT_MAX, mid = INT_MAX;
 
-        int j = INT_MAX, k = INT_MAX;
-        for (auto n : nums) {
-            if (n <= j)
-                j = n;
-            else if (n <= k)
-                k = n;
-            else
-                return true;
+        for (int n : nums) {
+            if (n <= low) low = n;
+            else if (n <= mid) mid = n;
+            else return true;
         }
 
         return false;
