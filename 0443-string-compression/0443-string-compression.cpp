64c7f3111527efa76c969bd44e912a6c5d91140b
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-12 15:09:24 -0700

    Time: 4 ms (70.7%), Space: 13.9 MB (68.84%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -3,8 +3,9 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int toWrite = 0, ans = 0;
+        int i = 0;
 
-        for (int i=0; i<chars.size();) {
+        while (i < chars.size()) {
             int length = 1;
             while (i + length < chars.size() and chars[i+length] == chars[i])
                 length++;
