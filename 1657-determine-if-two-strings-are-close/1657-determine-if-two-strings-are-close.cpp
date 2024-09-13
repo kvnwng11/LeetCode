6f12a97588530e39550d03da1b4678d5495ff331
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-13 13:35:52 -0700

    Time: 61 ms (84.88%), Space: 22.1 MB (94.22%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if (word1.size() != word2.size()) return false;
+        vector<int> count1(26, 0);
+        vector<int> count2(26, 0);
+
+        for (int i=0; i<word1.size(); ++i) {
+            count1[word1[i] - 'a']++;
+            count2[word2[i] - 'a']++;
+        }
+
+        for (int i=0; i<26; ++i) {
+            if (count1[i] == 0 and count2[i] > 0 || count1[i] > 0 and count2[i] == 0)
+                return false;
+        }
+
+        sort(count1.begin(), count1.end());
+        sort(count2.begin(), count2.end());
+
+        return count1 == count2;
+    }
+};
