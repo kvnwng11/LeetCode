c5178fd9f546f02e5daaf18bc1e6f0ef1c1d65a1
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-21 18:28:04 -0400

    Time: 62 ms (95.88%), Space: 62 MB (71.46%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,18 +1,46 @@
 class Solution {
+private:
+    map<pair<int, bool>, int> memo;
+    int curr = 0;
+    int ans = 0;
+
+    int solve(vector<int>& prices, int fee, int idx, bool holding) {
+        if (idx == prices.size())
+            return 0;
+        if (memo.find({idx, holding}) != memo.end()) 
+            return memo[{idx, holding}];
+
+        int pick, notpick;
+
+        if (!holding) {
+            pick = -prices[idx] + solve(prices, fee, idx+1, false);
+            notpick = solve(prices, fee, idx+1, holding);
+        }
+        else {
+            pick = solve(prices, fee, idx+1, holding);
+            notpick = prices[idx] + solve(prices, fee, idx+1, true);
+        }
+
+        int currProfit = max(pick, notpick);
+        memo[{idx, holding}] = currProfit;
+        ans = max(currProfit, ans);
+        return currProfit;
+    }
+
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
 
-        int free = 0;
-        int holding = -prices[0];
+        vector<int> free(n, 0);
+        vector<int> holding(n, 0);
+
+        holding[0] = -prices[0];
 
         for (int i=1; i<n; ++i) {
-            int oldHolding = holding;
-
-            holding = max(holding, free - prices[i]);
-            free = max(free, oldHolding + prices[i] - fee);
+            free[i] = max(free[i-1], holding[i-1] + prices[i] - fee);
+            holding[i] = max(holding[i-1], free[i-1] - prices[i]);
         }
 
-        return free;
+        return max(holding[n-1], free[n-1]);
     }
 };
