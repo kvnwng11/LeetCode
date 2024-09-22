919a97e6d3ce1bcd2004766f93b2ea433723433a
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-22 15:08:00 -0400

    Time: 246 ms (46.19%), Space: 93.7 MB (51.25%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,26 +1,20 @@
-bool MyCompare(vector<int> &v1, vector<int> &v2) {
-    return v1[1] < v2[1];
-}
-
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), MyCompare);
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int> &b) {
+            return a[1] < b[1];
+        });
 
-        int end = INT_MIN;
         int ans = 0;
-        for (int i=0; i<intervals.size(); ++i) {
-            int currStart = intervals[i][0];
-            int currEnd = intervals[i][1];
-
-            if (currStart >= end) {
-                end = currEnd;
-            }
-            else {
+        int lastValid = 0;
+        for (int i=1; i<intervals.size(); ++i) {
+            if (intervals[i][0] < intervals[lastValid][1]) {
                 ans++;
             }
+            else {
+                lastValid = i;
+            }
         }
-
         return ans;
     }
 };
