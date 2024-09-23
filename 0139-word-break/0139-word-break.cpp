95b68af16ea24f6febbae873eaee15b75a452753
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-23 17:02:39 -0400

    Time: 0 ms (100%), Space: 9.8 MB (97.68%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -7,29 +7,18 @@ public:
         dp[0] = true;
 
         for (int i=1; i<=m; ++i) {
-            for (int j=0; j<n; ++j) {
-                string &word = wordDict[j];
+            for (string &word : wordDict) {
+                int size = word.size();
+                
+                if (i - size < 0) continue;
 
-                int idx = i - word.size();
-                int idx2 = 0;
-                if (idx < 0) continue;
-                if (idx > 0 and !dp[idx]) continue;
-
-                bool nextWord = false;
-                while (idx < i) {
-                    char c1 = s[idx];
-                    char c2 = word[idx2];
-                    if (c1 != c2) {
-                        nextWord = true;
+                if (dp[i - size]) {
+                    if (s.substr(i - size, size) == word) {
+                        dp[i] = true;
                         break;
                     }
-                    idx++;
-                    idx2++;
                 }
-                        
-                if (nextWord) continue;
 
-                dp[i] = true;
             }
         }
 
