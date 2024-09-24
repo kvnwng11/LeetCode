433eef10b5011d76a2aa8dcb4cff07e0c60ede1b
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-24 15:42:28 -0400

    Time: 7 ms (91.69%), Space: 20.8 MB (37.69%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+
+        // Initialize
         vector<vector<int>> ans;
-        int i=0;
+        int i = 0;
         int n = intervals.size();
-        
-        while (i < n and intervals[i][1] < newInterval[0])
-            ans.push_back(intervals[i++]);
-        
-        while (i < n and intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = min(intervals[i][0], newInterval[0]);
-            newInterval[1] = max(intervals[i][1], newInterval[1]);
+
+        // Front non-overlapping
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        // Overlapping
+        while (i < n && newInterval[1] >= intervals[i][0]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
             i++;
         }
         ans.push_back(newInterval);
-        
-        while (i<n) 
-            ans.push_back(intervals[i++]);
-        
-    
+
+        // Back non-overlapping
+        while (i < n) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
         return ans;
     }
 };
