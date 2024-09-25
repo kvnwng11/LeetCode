051810da8df3b073f36796904ce54780feb601ab
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-25 08:55:08 -0400

    Time: 0 ms (100%), Space: 9 MB (55.15%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size()-1;
+
+        while (left < right) {
+            if (!isalpha(s[left]) && !isdigit(s[left])) left++;
+            else if (!isalpha(s[right]) && !isdigit(s[right])) right--;
+            else if (tolower(s[left]) != tolower(s[right])) return false;
+            else {
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+};
