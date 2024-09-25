6517c980ff8b41560cc7a5268ba7f71a7d6c4ee1
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-25 09:50:32 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int expand(string &s, int left, int right) {
+        int n = s.size();
+
+        while (left-1 >= 0 and right+1 < n and s[left-1] == s[right+1]) {
+            left--;
+            right++;
+        }
+        
+        return left == right? 1 : right - left;
+    }
+public:
+    int countSubstrings(string s) {
+        int ans = 0;
+
+        int n = s.size();
+        for (int i=0; i<n; ++i) {
+            // Odd length
+            ans += expand(s, i, i);
+
+            // Even length
+            if (i+1 < n && s[i] == s[i+1])
+                ans += expand(s, i, i+1);
+        }
+
+        return ans;
+    }
+};
