0ffda91e21316b2b6da3ef69710fe4da217e17e9
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-25 10:48:33 -0400

    Time: 36 ms (18.41%), Space: 15.8 MB (5.04%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -11,21 +11,34 @@ public:
 
         int minLength = INT_MAX;
         int minStart = -1;
+        
+        struct Element {
+            int index;
+            char char_val;
+        };
+        vector<Element> filteredS;
+        for (int i=0; i<s.size(); ++i) {
+            char c = s[i];
+            if (tCounts.find(c) != tCounts.end())
+                filteredS.push_back({i, c});
+        }
 
         int left = 0, right;
         int formed = 0, required = tCounts.size();
-        for (right = 0; right < s.size(); ++right) {
-            int c = s[right];
+        for (right = 0; right < filteredS.size(); ++right) {
+            char c = filteredS[right].char_val;
             sCounts[c] += 1;
 
             if (tCounts.find(c) != tCounts.end() && sCounts[c] == tCounts[c])
                 formed++;
 
             while (left <= right && formed == required) {
-                c = s[left];
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
-                    minStart = left;
+                c = filteredS[left].char_val;
+                int end = filteredS[right].index;
+                int start = filteredS[left].index;
+                if (end - start + 1 < minLength) {
+                    minLength = end - start + 1;
+                    minStart = start;
                 }
 
                 sCounts[c] -= 1;
