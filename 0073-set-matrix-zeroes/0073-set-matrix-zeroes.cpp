4c0d31a32e68f489dbf8adc0b6ab7adbc4163db1
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-24 20:15:07 -0400

    Time: 12 ms (40.41%), Space: 17.1 MB (19.03%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool needToChangeFirstCol = false;
         int m = matrix.size();
         int n = matrix[0].size();
-            
+        vector<bool> checkedRows(m);
+        vector<bool> checkedCols(n);
+
         for (int i=0; i<m; ++i) {
-            if (matrix[i][0] == 0)
-                needToChangeFirstCol = true;
-            
-            for (int j=1; j<n; ++j) {
+            for (int j=0; j<n; ++j) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    checkedRows[i] = true;
+                    checkedCols[j] = true;
                 }
             }
         }
-        
-        for (int i=1; i<m; ++i) {
-            for (int j=1; j<n; ++j) {
-                if (matrix[i][0] == 0 or matrix[0][j] == 0)
+
+
+        for (int i=0; i<m; ++i)
+            if (checkedRows[i])
+                for (int j=0; j<n; ++j)
                     matrix[i][j] = 0;
-            }
-        }
-        
-        if (matrix[0][0] == 0){
-            for (int j=0; j<n; ++j)
-                matrix[0][j] = 0;
-        }
-        
-        if (needToChangeFirstCol)
-            for (int i=0; i<m; ++i)
-                matrix[i][0] = 0;
-        
+
+        for (int j=0; j<n; ++j)
+            if (checkedCols[j])
+                for (int i=0; i<m; ++i)
+                    matrix[i][j] = 0;
     }
 };
