9fdcbbe273edb68a46da86b15a7cf0673ddfb961
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-25 11:24:38 -0400

    Time: 27 ms (7.19%), Space: 30.9 MB (5.46%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,25 +11,33 @@
  */
 class Solution {
 private:
-    void serialize(TreeNode* node, string &str) {
-        if (!node) {
-            str += '#';
-            return;
-        }
+    const int MOD_1 = 10e7 + 7;
+    const int MOD_2 = INT_MAX;
 
-        str += '^';
-        str += to_string(node->val);
-        serialize(node->left, str);
-        serialize(node->right, str);
+    vector<pair<unsigned long long, unsigned long long>> memo;
+
+    pair<unsigned long long, unsigned long long> hashTree(TreeNode* node, bool needToAdd) {
+        if (node == nullptr) return {3, 7};
+
+        auto left = hashTree(node->left, needToAdd);
+        auto right = hashTree(node->right, needToAdd);
+
+        auto left1 = (left.first << 5) % MOD_1;
+        auto right1 = (right.first << 1) % MOD_1;
+        auto left2 = (left.second << 7) % MOD_2;
+        auto right2 = (right.second << 1) % MOD_2;
+
+        pair hashpair = {(left1 + right1 + node->val) % MOD_1, (left2 + right2 + node->val) % MOD_2};
+        if (needToAdd) memo.push_back(hashpair);
+        return hashpair;
     }
 
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        string s = "";
-        serialize(root, s);
-        string t = "";
-        serialize(subRoot, t);
+        hashTree(root, true);
 
-        return s.find(t) != string::npos;
+        auto s = hashTree(subRoot, false);
+
+        return find(memo.begin(), memo.end(), s) != memo.end();
     }
 };
