216942e9e845410bd1adc9885dfeba0df51a5734
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-09-24 20:22:08 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,27 +3,20 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<bool> checkedRows(m);
-        vector<bool> checkedCols(n);
 
         for (int i=0; i<m; ++i) {
             for (int j=0; j<n; ++j) {
                 if (matrix[i][j] == 0) {
-                    checkedRows[i] = true;
-                    checkedCols[j] = true;
+
+                    // set col
+                    for (int row=0; row<m; ++row)
+                        matrix[row][j] = 0;
+
+                    // set row
+                    for (int row=0; row<n; ++row)
+                        matrix[i][row] = 0;
                 }
             }
         }
-
-
-        for (int i=0; i<m; ++i)
-            if (checkedRows[i])
-                for (int j=0; j<n; ++j)
-                    matrix[i][j] = 0;
-
-        for (int j=0; j<n; ++j)
-            if (checkedCols[j])
-                for (int i=0; i<m; ++i)
-                    matrix[i][j] = 0;
     }
 };
