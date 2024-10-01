e57f65a656a9936dd2b01b2cfea9cb2881e271da
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-10-01 09:15:23 -0400

    Time: 2 ms (63.26%), Space: 9.4 MB (44.88%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         unordered_map<char, int> counts;
-        int ans = 0;
 
+        int ans = 0;
+        int numOnes = 0;
         for (char c : s) {
             counts[c] += 1;
 
             if (counts[c] == 2) {
                 ans += 2;
+                numOnes--;
                 counts[c] = 0;
+            }
+            else if (counts[c] == 1) {
+                numOnes++;
             }
         }
 
-        return ans == s.size()? ans : ans+1;
+        return ans + (bool) numOnes;
     }
 };
