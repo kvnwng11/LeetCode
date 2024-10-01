3bf2c75ff436784ca4a21b9fcd7c2903c012d439
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-10-01 15:37:53 -0400

    Time: 16 ms (42.46%), Space: 11.5 MB (35.1%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,42 +1,38 @@
 class Solution {
-private:
-    bool equal(int* a1, int* a2) {
-        for (int i=0; i<26; ++i) {
-            if (a1[i] != a2[i])
-                return 0;
-        }
-        return 1;
-    }
-
 public:
     vector<int> findAnagrams(string s, string p) {
-        if (s.size() < p.size()) return {};
-
         vector<int> ans;
-        
-        int sChar[26] = {0};
-        int pChar[26] = {0};
+        if (p.size() > s.size()) return ans;
 
-        // Initialize
-        for (int i=0; i<p.size(); ++i) {
-            pChar[(p[i] - 'a')]++;
+        unordered_map<char, int> pCounts;
+        unordered_map<char, int> windowCounts;
+
+        for (int right = 0; right < p.size(); ++right) {
+            pCounts[p[right]] += 1;
         }
+        
+        int left = 0;
+        int formed = 0;
+        int required = pCounts.size();
+        for (int right = 0; right < s.size(); ++right) {
+            char c = s[right];
+            windowCounts[c] += 1;
 
-        // One pass
-        for (int i=0; i<s.size(); ++i) {
-            // Remove from window
-            if (i >= p.size())
-                sChar[(s[i - p.size()] - 'a')]--;
+            if (windowCounts[c] == pCounts[c])
+                formed++;
+            
+            if (right >= p.size() - 1) {
+                if (formed == required)
+                    ans.push_back(left);
 
-            // Add to window
-            sChar[(s[i] - 'a')]++;
-
-            // Check if equal
-            if (i >= p.size()-1 and equal(sChar, pChar)) {
-                ans.push_back(i - p.size() + 1);
+                c = s[left];
+                if (windowCounts[c] == pCounts[c])
+                    formed--;
+                windowCounts[c] -= 1;
+                left++;
             }
         }
-
+    
         return ans;
     }
 };
