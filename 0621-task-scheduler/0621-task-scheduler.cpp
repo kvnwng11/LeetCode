74315100a257f7c8748b4581e98768e70988fc7e
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-10-02 15:07:55 -0400

    Time: 39 ms (97.05%), Space: 38.3 MB (68.81%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int largest = 0;
         unordered_map<char, int> counts;
+        int biggest = 0;
         for (char c : tasks) {
             counts[c] += 1;
-            largest = max(counts[c], largest);
+            biggest = max(biggest, counts[c]);
         }
+        int ans = (biggest-1) * (n+1);
+        for (auto &[c, freq] : counts)
+            if (freq == biggest)
+                ans++;
 
-        int freq = 0;
-        for (auto &p : counts) {
-            if (p.second == largest)
-                freq++;
-        }
-
-        return max((int)tasks.size(), (int)((largest - 1) * (n + 1) + freq));
+        return max((int) tasks.size(), ans);
     }
 };
