8776c6f03b83e686d2d8ffc8e2ab5b0ff4af3d12
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-10-02 15:43:25 -0400

    Time: 0 ms (100%), Space: 12.8 MB (63.65%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,27 +1,43 @@
+struct TrieNode {
+    unordered_map<char, TrieNode*> children;
+    bool isWord = false;
+};
+
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        TrieNode* root = new TrieNode();
+        for (string& word : wordDict) {
+            TrieNode* curr = root;
+            for (char c : word) {
+                if (!curr->children.count(c))
+                    curr->children[c] = new TrieNode();
+                curr = curr->children[c];
+            }
+            curr->isWord = true;
+        }
+
+
         int m = s.size();
         int n = wordDict.size();
-        vector<bool> dp(m+1);
-        dp[0] = true;
+        vector<bool> dp(m);
 
-        for (int i=1; i<=m; ++i) {
-            for (string &word : wordDict) {
-                int size = word.size();
-                
-                if (i - size < 0) continue;
+        for (int i=0; i<m; ++i) {
+            if (i == 0 || dp[i-1]) {
+                TrieNode* curr = root;
+                for (int j=i; j<m; ++j) {
+                    char c = s[j];
 
-                if (dp[i - size]) {
-                    if (s.substr(i - size, size) == word) {
-                        dp[i] = true;
+                    if (!curr->children.count(c))
                         break;
-                    }
+                    
+                    curr = curr->children[c];
+                    if (curr->isWord)
+                        dp[j] = true;
                 }
-
             }
         }
 
-        return dp[m];
+        return dp[m-1];
     }
 };
