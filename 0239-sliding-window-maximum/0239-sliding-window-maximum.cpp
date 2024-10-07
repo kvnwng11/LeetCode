363477d81df9c23fd3f2282dbf9c5e7c9c4639f2
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-10-07 11:33:23 -0400

    Time: 174 ms (50.5%), Space: 137.4 MB (34.02%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        unordered_map<int, int> counts;
-        priority_queue<int> pq;
         vector<int> ans;
+        deque<int> d;
 
         for (int i=0; i<nums.size(); ++i) {
-            pq.push(nums[i]);
-            counts[nums[i]] += 1;
+            while (!d.empty() && d.front() <= i - k) d.pop_front();
+            while (!d.empty() && nums[d.back()] < nums[i]) d.pop_back();
 
-            if (pq.size() >= k) {
-
-                while (counts[pq.top()] == 0)
-                    pq.pop();
-
-                ans.push_back(pq.top());
-
-                counts[nums[i-k+1]] -= 1;
-            }
+            d.push_back(i);
+            if (i >= k-1) ans.push_back(nums[d.front()]);
         }
 
 
