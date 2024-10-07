37f1da1bc396a189b6bb2c76820e455f86c69201
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-10-07 14:35:05 -0400

    Time: 4 ms (44%), Space: 15.7 MB (30.09%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,30 +1,17 @@
 class Solution {
-private:
-    vector<string> ans;
-
-    void backtrack(string &curr, int leftCount, int rightCount, int n) {
-        if (curr.size() == 2 * n) {
-            ans.push_back(curr);
-            return;
-        }
-
-        if (leftCount < n) {
-            curr.push_back('(');
-            backtrack(curr, leftCount+1, rightCount, n);
-            curr.pop_back();
-        }
-
-        if (leftCount > rightCount) {
-            curr.push_back(')');
-            backtrack(curr, leftCount, rightCount+1, n);
-            curr.pop_back();
-        }
-    }
-
 public:
     vector<string> generateParenthesis(int n) {
-        string curr = "";
-        backtrack(curr, 0, 0, n);
+        if (n == 0)
+            return {""};
+        
+        vector<string> ans;
+        for (int leftCount = 0; leftCount < n; ++leftCount) {
+            for (string &leftString : generateParenthesis(leftCount)) {
+                for (string &rightString : generateParenthesis(n - 1 - leftCount))
+                ans.push_back("(" + leftString + ")" + rightString);
+            }
+        }
+
         return ans;
     }
 };
