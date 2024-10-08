4c1128ff87e8068660b8ee3fb9ac0186d0c796d4
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-10-08 16:47:48 -0400

    Time: 275 ms (83.99%), Space: 21.1 MB (22.37%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,73 +1,68 @@
-struct TrieNode {
-    vector<TrieNode*> children;
-    string word = "";
-
-    TrieNode() : children{vector<TrieNode*>(26)} {}
-};
-
-
 class Solution {
 private:
+    struct TrieNode {
+        vector<TrieNode*> children;
+        string word = "";
+        bool isWord = false;
+
+        TrieNode() { children = vector<TrieNode*>(26, nullptr); }
+    };
+
     vector<string> ans;
+    TrieNode* root = new TrieNode();
     int DIRECTIONS[4][2] = {
         {0, 1},
         {0, -1},
         {1, 0},
         {-1, 0}
     };
+    int m;
+    int n;
 
-    TrieNode* root = nullptr;
-
-    bool inBounds(int row, int col, vector<vector<char>>& board) {
-        return row >= 0 && row < board.size() && col >= 0 && col < board[0].size();
+    bool inBounds(int i, int j) {
+        return 0 <= i && i < m && 0 <= j && j < n;
     }
 
-    void backtrack(TrieNode* parent, vector<vector<char>>& board, int row, int col) {
-        char c = board[row][col];
-        if (c == '#') return;
-        if (!parent->children[c - 'a']) return;
-
-        TrieNode* curr = parent->children[c - 'a'];
-        if (!curr->word.empty()) {
+    void dfs(vector<vector<char>>& board, int row, int col, TrieNode* curr) {
+        if (!curr) return;
+        if (curr->word.size() > 0) {
             ans.push_back(curr->word);
             curr->word = "";
         }
-
-        board[row][col] = '#';
+        char orig = board[row][col];
+        board[row][col] = '.';
 
         for (auto dir : DIRECTIONS) {
             int newRow = row + dir[0];
             int newCol = col + dir[1];
 
-            if (inBounds(newRow, newCol, board)) {
-                backtrack(curr, board, newRow, newCol);
+            if (inBounds(newRow, newCol) && board[newRow][newCol] != '.' 
+                        && curr->children[board[newRow][newCol] - 'a']) {
+                dfs(board, newRow, newCol, curr->children[board[newRow][newCol] - 'a']);
             }
         }
 
-        board[row][col] = c;
+        board[row][col] = orig;
     }
 
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        TrieNode* root = new TrieNode();
         for (string &word : words) {
             TrieNode* curr = root;
             for (char c : word) {
                 if (!curr->children[c - 'a'])
                     curr->children[c - 'a'] = new TrieNode();
-                curr = curr->children[c-'a'];
+                curr = curr->children[c - 'a'];
             }
             curr->word = word;
         }
 
-
-        int m = board.size();
-        int n = board[0].size();
+        m = board.size();
+        n = board[0].size();
         for (int i=0; i<m; ++i) {
             for (int j=0; j<n; ++j) {
-                char c = board[i][j];
-                if (root->children[c - 'a'])
-                    backtrack(root, board, i, j);
+                if (root->children[board[i][j] - 'a']) 
+                    dfs(board, i, j, root->children[board[i][j] - 'a']);
             }
         }
 
