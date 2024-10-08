696cec5d1babaf1658a2eee13f7df67fb2e98d6c
Kevin Wang <71472955+kvnwng11@users.noreply.github.com>
2024-10-08 15:32:24 -0400

    Time: 94 ms (60.68%), Space: 79.5 MB (33.11%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -3,25 +3,18 @@ private:
     vector<vector<string>> ans;
     vector<string> curr;
 
-    bool isPalindrome(string &s) {
-        int left=0, right = s.size()-1;
-        while (left <= right)
-            if (s[left++] != s[right--]) return false;
-        return true;
-    }
-
-    void backtrack(string &s, int idx, string &currString) {
-        if (idx == s.size()) {
+    void backtrack(string &s, int start, vector<vector<bool>> &dp) {
+        if (start == s.size()) {
             ans.push_back(curr);
             return;
         }
 
-        for (int i=idx; i<s.size(); ++i) {
-            currString += s[i];
-            if (isPalindrome(currString)) {
-                curr.push_back(currString);
-                string newString = "";
-                backtrack(s, i+1, newString);
+        for (int end = start; end < s.size(); ++end) {
+            if (s[start] == s[end] && (end-start <= 2 || dp[start+1][end-1])) {
+                dp[start][end] = true;
+
+                curr.push_back(s.substr(start, end - start + 1));
+                backtrack(s, end+1, dp);
                 curr.pop_back();
             }
         }
@@ -29,8 +22,9 @@ private:
 
 public:
     vector<vector<string>> partition(string s) {
-        string curr = "";
-        backtrack(s, 0, curr);
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n));
+        backtrack(s, 0, dp);
         return ans;
     }
 };
